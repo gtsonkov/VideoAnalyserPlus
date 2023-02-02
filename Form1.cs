1407908a19228979b73ff92839e214e5474ad5fc
gtsonkov <gtsonkov@abv.bg>
2023-02-02 22:26:11 +0100

    add old TrackColor method (comented)

--- Form1.cs
+++ Form1.cs
@@ -64,6 +64,78 @@ namespace VideoAnalyserPlus
             //this._trackingThread.Abort();
         }
 
+        //private void TrackColor()
+        //{
+        //    while (!this.stopThread)
+        //    {
+        //        this._capture.Read(_frame);
+        //
+        //        if (this._frame.IsEmpty)
+        //        {
+        //            break;
+        //        }
+        //        
+        //        //To Do: Use RGB
+        //        Mat hsv = new Mat();
+        //        CvInvoke.CvtColor(_frame, hsv, ColorConversion.Bgr2Hsv);
+        //
+        //        //To Do: Implement new Method to avoid copy code (at the moment just for test)
+        //        #region First Color
+        //
+        //        //To Do: Make it dynamic 
+        //        var lower = new ScalarArray(new MCvScalar(0, 0, 0));
+        //        var upper = new ScalarArray(new MCvScalar(200, 255, 255));
+        //
+        //        Mat mask = new Mat();
+        //
+        //        CvInvoke.CvtColor(_frame, hsv, ColorConversion.Bgr2Hsv);
+        //        CvInvoke.InRange(hsv, lower, upper, mask);
+        //
+        //        VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+        //        Mat hierarchy = new Mat();
+        //        CvInvoke.FindContours(mask, contours, hierarchy, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+        //
+        //        for (int i = 0; i < contours.Size; i++)
+        //        {
+        //            Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+        //
+        //            //To Do: Make minimum size (rect.Height & rect.Width) dynamic
+        //            if (rect.Height > 5 && rect.Width > 5)
+        //            {
+        //                CvInvoke.Rectangle(_frame, rect, new MCvScalar(0, 255, 0), 2);
+        //            }
+        //        }
+        //        #endregion
+        //
+        //        #region Second Color
+        //
+        //        //To Do: Make it dynamic 
+        //        lower = new ScalarArray(new MCvScalar(20, 25, 10));
+        //        upper = new ScalarArray(new MCvScalar(100, 150, 105));
+        //
+        //        CvInvoke.CvtColor(_frame, hsv, ColorConversion.Bgr2Hsv);
+        //        CvInvoke.InRange(hsv, lower, upper, mask);
+        //
+        //        VectorOfVectorOfPoint contours2 = new VectorOfVectorOfPoint();
+        //        Mat hierarchy2 = new Mat();
+        //        CvInvoke.FindContours(mask, contours2, hierarchy2, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+        //
+        //        for (int i = 0; i < contours2.Size; i++)
+        //        {
+        //            Rectangle rect = CvInvoke.BoundingRectangle(contours2[i]);
+        //
+        //            //To Do: Make rectangle color dynamic
+        //            CvInvoke.Rectangle(_frame, rect, new MCvScalar(255, 255, 255), 2);
+        //        }
+        //        #endregion
+        //
+        //        //screenBox.Image = BitmapExtension.ToBitmap(this._frame);
+        //
+        //        CvInvoke.Imshow("VT-Color Tracker", this._frame);
+        //        CvInvoke.WaitKey(this._frameRate);
+        //    }
+        //}
+
         private void TrackColor()
         {
             while (!stopThread)
