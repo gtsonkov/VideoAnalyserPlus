2a4275893f32a9455af8e7805c11ed3c515e6066
gtsonkov@abv.bg <gtsonkov@abv.bg>
2023-02-03 12:44:37 +0100

    add new Form for Video Player

---
+++ VideoPlayerForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace VT
+{
+    partial class VideoPlayerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PlayBtn = new System.Windows.Forms.Button();
+            this.PauseBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(794, 479);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PlayBtn
+            // 
+            this.PlayBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.PlayBtn.Location = new System.Drawing.Point(286, 494);
+            this.PlayBtn.Name = "PlayBtn";
+            this.PlayBtn.Size = new System.Drawing.Size(75, 23);
+            this.PlayBtn.TabIndex = 1;
+            this.PlayBtn.Text = "Play";
+            this.PlayBtn.UseVisualStyleBackColor = true;
+            this.PlayBtn.Click += new System.EventHandler(this.PlayBtn_Click);
+            // 
+            // PauseBtn
+            // 
+            this.PauseBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.PauseBtn.Location = new System.Drawing.Point(377, 494);
+            this.PauseBtn.Name = "PauseBtn";
+            this.PauseBtn.Size = new System.Drawing.Size(75, 23);
+            this.PauseBtn.TabIndex = 2;
+            this.PauseBtn.Text = "Pause";
+            this.PauseBtn.UseVisualStyleBackColor = true;
+            this.PauseBtn.Click += new System.EventHandler(this.PauseBtn_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.StopBtn.Location = new System.Drawing.Point(468, 494);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(75, 23);
+            this.StopBtn.TabIndex = 3;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // VideoPlayerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 536);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.PauseBtn);
+            this.Controls.Add(this.PlayBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "VideoPlayerForm";
+            this.Text = "VideoPlayerForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button PlayBtn;
+        private Button PauseBtn;
+        private Button StopBtn;
+    }
+}
---
+++ VideoPlayerForm.cs
@@ -0,0 +1,25 @@
+﻿namespace VT
+{
+    public partial class VideoPlayerForm : Form
+    {
+        public VideoPlayerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PauseBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
