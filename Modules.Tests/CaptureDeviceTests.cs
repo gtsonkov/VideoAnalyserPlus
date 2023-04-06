833c6b52669012488dc3a70d0617df45b9ca919d
gtsonkov <gtsonkov@abv.bg>
2023-04-06 07:54:58 +0200

    edit UnitTests for CaptureDevice

--- Modules.Tests/CaptureDeviceTests.cs
+++ Modules.Tests/CaptureDeviceTests.cs
@@ -1,4 +1,3 @@
-using DirectShowLib;
 using Modules.Interfaces;
 using Moq;
 
@@ -7,17 +6,20 @@ namespace Modules.Tests
     public class CaptureDeviceTests
     {
         private Mock<IDsDeviceWrapper> _deviceWrapperMock;
+        private CaptureDevice _device;
+        private string originName = "Device01";
 
         [SetUp]
         public void Setup()
         {
             this._deviceWrapperMock = new Mock<IDsDeviceWrapper>();
+           //this._device = new CaptureDevice(this._deviceWrapperMock.Object,this.originName,0);
         }
 
         [Test]
         public void CaptureDevice_Constructor_NullSource_ThrowsException()
         {
-            Assert.Throws<ArgumentNullException>(() => new CaptureDevice(null, "someName", 0));
+            Assert.Throws<ArgumentNullException>(() => new CaptureDevice(null, "someName", 1));
         }
     
 
@@ -25,6 +27,18 @@ namespace Modules.Tests
         public void CaptureDevice_Constructor_EmptyDeviceName_ThrowsException()
         {
             Assert.Throws<ArgumentNullException>(() => new CaptureDevice(_deviceWrapperMock.Object, string.Empty, 0));
+        }
+
+        [Test]
+        public void CaptureDevice_Constructor_NegativePosition_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() => new CaptureDevice(_deviceWrapperMock.Object, this.originName, -1));
+        }
+
+        [Test]
+        public void CaptureDevice_Set_Name_Set_Correctly_From_Constructor()
+        {
+            Assert.AreEqual(this.originName, this._device.DeviceName);
         }
     }
 }
