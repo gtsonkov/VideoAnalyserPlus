c6ab5fd9ccab733ae36d669c09806ebc8b25eb09
gtsonkov <gtsonkov@abv.bg>
2024-02-17 17:26:48 +0100

    -init DetectionArea class

---
+++ Modules/Interfaces/IDetectionArea.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace Modules.Interfaces
+{
+    public interface IDetectionArea
+    {
+        int Height { get; set; }
+        string Label { get; set; }
+        int PixelSize { get; }
+        Size Size { get; set; }
+        int Width { get; set; }
+        int X { get; set; }
+        int Y { get; set; }
+    }
+}
---
+++ Modules/Models/DetectionArea.cs
@@ -0,0 +1,137 @@
+﻿using Modules.Interfaces;
+using System.Drawing;
+
+namespace Modules.Models
+{
+    public class DetectionArea : IDetectionArea
+    {
+        private Rectangle rectangle;
+
+        public DetectionArea(int x, int y, int width, int height, string label)
+        {
+            this.rectangle = new Rectangle(x, y, width, height);
+            this.Label = label;
+        }
+
+        public DetectionArea(string label)
+        {
+            this.rectangle = new Rectangle();
+            this.Label = label;
+        }
+
+        public int Width
+        {
+            get
+            {
+                return this.rectangle.Width;
+            }
+
+            set
+            {
+                try
+                {
+                    this.rectangle.Width = value;
+                }
+                catch (Exception ex)
+                {
+
+                    throw new ArgumentException(ex.Message);
+                }
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return this.rectangle.Height;
+            }
+
+            set
+            {
+                try
+                {
+                    this.rectangle.Height = value;
+                }
+                catch (Exception ex)
+                {
+
+                    throw new ArgumentException(ex.Message);
+                }
+            }
+        }
+
+        public int X
+        {
+            get
+            {
+                return this.rectangle.X;
+            }
+
+            set
+            {
+                try
+                {
+                    this.rectangle.X = value;
+                }
+                catch (Exception ex)
+                {
+
+                    throw new ArgumentException(ex.Message);
+                }
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return this.rectangle.Y;
+            }
+
+            set
+            {
+                try
+                {
+                    this.rectangle.Y = value;
+                }
+                catch (Exception ex)
+                {
+
+                    throw new ArgumentException(ex.Message);
+                }
+            }
+        }
+
+        public Size Size
+        {
+            get
+            {
+                return this.rectangle.Size;
+            }
+
+            set
+            {
+                try
+                {
+                    this.rectangle.Size = value;
+                }
+                catch (Exception ex)
+                {
+
+                    throw new ArgumentException(ex.Message);
+                }
+            }
+        }
+
+        public int PixelSize
+        {
+            get
+            {
+                return this.rectangle.X * this.rectangle.Height;
+            }
+        }
+
+        public string Label { get; set; }
+    }
+}
